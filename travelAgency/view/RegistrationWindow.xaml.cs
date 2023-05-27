7467dd734c96dd2989ed3e9fcbbaf3eae637900e
jovannajdovski <j.najdovski@gmail.com>
2023-05-27 23:52:51 +0200

    Registration view added

---
+++ travelAgency/view/RegistrationWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace travelAgency.view
+{
+    /// <summary>
+    /// Interaction logic for RegistrationWindow.xaml
+    /// </summary>
+    public partial class RegistrationWindow : Window
+    {
+        public RegistrationWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Register_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void To_Login(object sender, MouseButtonEventArgs e)
+        {
+            //LoginWindow loginWindow = new LoginWindow();
+            //loginWindow.Show();
+            //Close();
+        }
+    }
+}
