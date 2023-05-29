26f4ff1259e516d453cde9172bea1d1e19b1bed3
Nikola1709 <savickosovska@gmail.com>
2023-05-29 18:04:41 +0200

    Revert "Delete some images"
    This reverts commit 2cc380770653119e332d6d8cd96a1a9ba7cc84a8.

---
+++ travelAgency/model/TripSchedule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace travelAgency.model;
+
+public class TripSchedule
+{
+    [Key]
+    public int Id { get; set; }
+    public DateTime DateTime { get; set; }
+    public int TripId { get; set; }
+    public virtual Trip Trip { get; set; }
+    public int PlaceId { get; set; }
+    public virtual Place Place { get; set; }
+
+}
