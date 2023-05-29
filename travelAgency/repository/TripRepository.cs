b2fc5229cfb13ea3f53329049a6e894a810fd238
jovannajdovski <j.najdovski@gmail.com>
2023-05-29 21:23:48 +0200

    Trip cards added

---
+++ travelAgency/repository/TripRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using travelAgency.model;
+
+namespace travelAgency.repository
+{
+    public class TripRepository
+    {
+        private readonly TravelAgencyContext dbContext;
+
+        public TripRepository(TravelAgencyContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public Trip GetById(int id)
+        {
+            return dbContext.Trips.FirstOrDefault(u => u.Id == id);
+        }
+
+        public List<Trip> GetAll()
+        {
+            return dbContext.Trips.ToList();
+        }
+
+        public void Add(Trip trip)
+        {
+            dbContext.Trips.Add(trip);
+            dbContext.SaveChanges();
+        }
+
+        public void Update(Trip trip)
+        {
+            dbContext.Trips.Update(trip);
+            dbContext.SaveChanges();
+        }
+
+        public void Delete(Trip trip)
+        {
+            dbContext.Trips.Remove(trip);
+            dbContext.SaveChanges();
+        }
+        
+    }
+}
---
+++ travelAgency/view/AgentHome.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using travelAgency.components;
+using travelAgency.controls;
+using travelAgency.model;
+using travelAgency.repository;
+
+namespace travelAgency.view
+{
+    /// <summary>
+    /// Interaction logic for AgentHome.xaml
+    /// </summary>
+    public partial class AgentHome : Window
+    {
+        public TravelAgencyContext dbContext;
+        public TripRepository tripRepository;
+        public AgentHome()
+        {
+
+            InitializeComponent();
+
+            dbContext = new TravelAgencyContext();
+            tripRepository = new TripRepository(dbContext);
+
+            List<Trip> trips = tripRepository.GetAll();
+            foreach(Trip t in trips)
+            {
+                TripCard tripCard = new TripCard
+                {
+                    Margin = new Thickness(10),
+                    TripName = t.Name,
+                    Route = t.Price.ToString(),
+                    Description = t.Description
+                };
+                cards.Children.Add(tripCard);
+            }
+
+            TripCard tripCard1 = new TripCard
+            {
+                Margin = new Thickness(10),
+                TripName = "Tura zapadna Srbija",
+                Route = "Šabac - Bajina Bašta",
+                Description = "Tura je veoma zaniljiva i duga jer Savic i drugari imaju sta da ponude i njihovom kraju"
+            };
+
+            TripCard tripCard2 = new TripCard
+            {
+                Margin = new Thickness(10),
+                TripName = "Planinski maratoni",
+                Route = "Raška - Pančićev vrh",
+                Description = "Tura je veoma zaniljiva i duga jer Savic i drugari imaju sta da ponude i njihovom kraju"
+            };
+
+            TripCard tripCard3 = new TripCard
+            {
+                Margin = new Thickness(10),
+                TripName = "Tura južna Srbija",
+                Route = "Vranje - Đavolja Varoš",
+                Description = "Ubedljiva najbolja tura u nasoj ponudi"
+            };
+
+            cards.Children.Add(tripCard1);
+            cards.Children.Add(tripCard2);
+            cards.Children.Add(tripCard3);
+        }
+
+        private void Search_OnKeyDown(object sender, KeyEventArgs e)
+        {
+            var textBox = (TextBox)sender;
+            if (e.Key == Key.Enter)
+                SearchButton.Command.Execute(textBox.Text);
+        }
+    }
+}
