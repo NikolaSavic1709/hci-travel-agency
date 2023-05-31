a0c236a333385ec287d4b6b23f2055aff6f3d60a
Nikola1709 <savickosovska@gmail.com>
2023-05-31 19:27:24 +0200

    Continue working on forms

---
+++ travelAgency/Dialogs/CreatePlaceDialog.xaml.cs
@@ -0,0 +1,57 @@
+﻿using DevExpress.Xpf.Core.Native;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace travelAgency.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for CreatePlaceDialog.xaml
+    /// </summary>
+    public partial class CreatePlaceDialog : Window
+    {
+        public CreatePlaceDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void OutlinedComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox comboBox = (ComboBox)sender;
+            string selected= comboBox.SelectedItem.ToString();
+            if (selected.Contains("Accomodation"))
+                AmenitiesFragment.Visibility = Visibility.Visible;
+            else
+                AmenitiesFragment.Visibility = Visibility.Hidden;
+        }
+    }
+    public class VisibilityToGridHeightConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var visibility = (Visibility)value;
+            return visibility == Visibility.Visible ? new GridLength(200) : new GridLength(0);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
--- travelAgency/Dialogs/CreateTripScheduleDialog.xaml.cs
+++ travelAgency/Dialogs/CreateTripScheduleDialog.xaml.cs
@@ -17,37 +17,23 @@ using travelAgency.ViewModel;
 namespace travelAgency.Dialogs
 {
     /// <summary>
-    /// Interaction logic for CreateTripDialog.xaml
+    /// Interaction logic for CreateTripScheduleDialog.xaml
     /// </summary>
-    public partial class CreateTripDialog : Window
+    public partial class CreateTripScheduleDialog : Window
     {
         List<Place> allPlaces;
-        CreateTripViewModel ViewModel { get; set; }
-        int currentIndexListBox=-1;
-
-        public CreateTripDialog()
+        int currentIndexListBox = -1;
+        public CreateTripScheduleDialog()
         {
             InitializeComponent();
-            DataContext = new CreateTripViewModel();
-            var viewModel = DataContext as CreateTripViewModel;
-            if (viewModel != null)
-            {
-                ViewModel = viewModel;
-                Trip trip = new Trip();
-                viewModel.Trip=trip;
-            }
+
             Place place1 = new Place();
             place1.Name = "Sabac - Srbija";
             Place place2 = new Place();
             place2.Name = "Novi Sad - Srbija";
             Place place3 = new Place();
             place3.Name = "Beograd";
-            allPlaces=new List<Place> { place1, place2, place3 };
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            this.Close();
+            allPlaces = new List<Place> { place1, place2, place3 };
         }
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
@@ -61,6 +47,10 @@ namespace travelAgency.Dialogs
             else
                 AutocompleteListBox.Visibility = Visibility.Hidden;
 
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
         private void TextBox_OnFocusLost(object sender, RoutedEventArgs e)
         {
@@ -83,33 +73,6 @@ namespace travelAgency.Dialogs
             Keyboard.ClearFocus();
             AutocompleteListBox.Visibility = Visibility.Hidden;
             currentIndexListBox = -1;
-        }
-
-        private void AddPlace_Click(object sender, RoutedEventArgs e)
-        {
-            Place? selectedPlace = allPlaces.Find(p => p.Name == PlaceTextBox.Text);
-            
-            if (selectedPlace != null)
-            {
-                TripSchedule tripSchedule = new TripSchedule();
-                tripSchedule.Place = selectedPlace;
-                DateTime? date=DatePicker.SelectedDate.Value;
-                DateTime? time = TimePicker.SelectedTime.Value;
-                
-                tripSchedule.DateTime=new DateTime(date.Value.Year,date.Value.Month,date.Value.Day,time.Value.Hour,time.Value.Minute,0);
-                ViewModel.Trip.Schedules.Add(tripSchedule);
-            }
-            else
-            {
-                //handle
-            }
-        }
-
-        private void RemovePlace_Click(object sender, RoutedEventArgs e)
-        {
-            Button removeButton = (Button)sender;
-            TripSchedule tripSchedule = (TripSchedule)removeButton.DataContext;
-            ViewModel.Trip.Schedules.Remove(tripSchedule);
         }
         private void TextBox_KeyDown(object sender, KeyEventArgs e)
         {
@@ -134,7 +97,7 @@ namespace travelAgency.Dialogs
                 if (AutocompleteListBox.Items.Count > 0)
                 {
                     if (currentIndexListBox == -1) currentIndexListBox = 0;
-                    currentIndexListBox = (currentIndexListBox - 1 + AutocompleteListBox.Items.Count) % AutocompleteListBox.Items.Count;
+                    currentIndexListBox = (currentIndexListBox - 1+AutocompleteListBox.Items.Count) % AutocompleteListBox.Items.Count;
                     Place? place = AutocompleteListBox.Items[currentIndexListBox] as Place;
                     if (place != null)
                     {
@@ -144,12 +107,12 @@ namespace travelAgency.Dialogs
                     }
                 }
             }
-            if (e.Key==Key.Return)
+            if (e.Key == Key.Return)
             {
                 e.Handled = true;
                 if (currentIndexListBox != -1)
                 {
-                    AutocompleteListBox.SelectedIndex= currentIndexListBox;
+                    AutocompleteListBox.SelectedIndex = currentIndexListBox;
                 }
             }
         }
