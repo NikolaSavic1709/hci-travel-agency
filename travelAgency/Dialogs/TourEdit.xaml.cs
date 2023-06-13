023dbdaa0917d79485676fa5225139d3d5aab26b
Nikola1709 <savickosovska@gmail.com>
2023-06-13 05:28:54 +0200

    Finished disable button for one form from validity

--- travelAgency/Dialogs/TourEdit.xaml.cs
+++ travelAgency/Dialogs/TourEdit.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Globalization;
+using System.Linq;
 using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
@@ -11,81 +13,103 @@ using travelAgency.Dialogs;
 using travelAgency.model;
 using travelAgency.repository;
 
-namespace travelAgency.view;
-
-/// <summary>
-/// Interaction logic for Window1.xaml
-/// </summary>
-public partial class TourEdit : Window
+namespace travelAgency.view
 {
-    private static readonly Regex _regex = new Regex("[^0-9.-]+"); //regex that matches disallowed text
-
-    TripRepository tripRepository;
-    private Trip trip;
-    private static bool IsTextAllowed(string text)
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class TourEdit : Window
     {
-        return !_regex.IsMatch(text);
-    }
+        private static readonly Regex _regex = new Regex("[^0-9.-]+"); //regex that matches disallowed text
 
-    private void TextBoxPasting(object sender, DataObjectPastingEventArgs e)
-    {
-        if (e.DataObject.GetDataPresent(typeof(String)))
+        TripRepository tripRepository;
+        private Trip trip;
+        private static bool IsTextAllowed(string text)
         {
-            String text = (String)e.DataObject.GetData(typeof(String));
-            if (!IsTextAllowed(text))
+            return !_regex.IsMatch(text);
+        }
+
+        private void TextBoxPasting(object sender, DataObjectPastingEventArgs e)
+        {
+            if (e.DataObject.GetDataPresent(typeof(String)))
+            {
+                String text = (String)e.DataObject.GetData(typeof(String));
+                if (!IsTextAllowed(text))
+                {
+                    e.CancelCommand();
+                }
+            }
+            else
             {
                 e.CancelCommand();
             }
         }
-        else
+        public string Name1 { get; set; }
+        public string Description { get; set; }
+        public string Price { get; set; }
+        public TourEdit(Trip? trip, TripRepository tripRepository)
         {
-            e.CancelCommand();
+            InitializeComponent();
+            this.tripRepository = tripRepository;
+            DataContext = this;
+            if (trip != null)
+            {
+                this.trip = trip;
+                Name1 = trip.Name;
+                Description = trip.Description;
+                Price = trip.Price.ToString();
+            }
+        }
+
+        private void Price_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsTextAllowed(e.Text);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        public event EventHandler<DialogResultEventArgs> DialogResultEvent;
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.trip.Name = NameTxtBox.Text;
+            this.trip.Description = DescriptionTxtBox.Text;
+            this.trip.Price = Convert.ToDouble(PriceTxtBox.Text);
+            tripRepository.Save();
+            DialogResultEvent?.Invoke(this, new DialogResultEventArgs(true));
+            Close();
+        }
+
+
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            BindingExpression bindingExpr = textBox.GetBindingExpression(TextBox.TextProperty);
+
+            // Manually trigger the validation
+            bindingExpr.UpdateSource();
         }
     }
-    public string Name1 { get; set; }
-    public string Description { get; set; }
-    public string Price { get; set; }
-    public TourEdit(Trip? trip, TripRepository tripRepository)
+
+    public class MultiTextBoxValidationConverter : IMultiValueConverter
     {
-        InitializeComponent();
-        this.tripRepository = tripRepository;
-        DataContext = this;
-        if (trip!=null)
+        //public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        //{
+        //   bool hasErrors = values.OfType<ValidationError>().Any(error => error.ErrorContent != null);
+        //    return !hasErrors;
+        //}
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
-            this.trip = trip;
-            Name1=trip.Name;
-            Description = trip.Description;
-            Price = trip.Price.ToString();
+            bool hasErrors = values.OfType<bool>().Any(value => value);
+            return !hasErrors;
         }
-    }
-
-    private void Price_PreviewTextInput(object sender, TextCompositionEventArgs e)
-    {
-        e.Handled = !IsTextAllowed(e.Text);
-    }
-
-    private void Button_Click(object sender, RoutedEventArgs e)
-    {
-        Close();
-    }
-    public event EventHandler<DialogResultEventArgs> DialogResultEvent;
-    private void Button_Click_1(object sender, RoutedEventArgs e)
-    {
-        this.trip.Name=NameTxtBox.Text;
-        this.trip.Description=DescriptionTxtBox.Text;
-        this.trip.Price=Convert.ToDouble(PriceTxtBox.Text) ;
-        tripRepository.Save();
-        DialogResultEvent?.Invoke(this, new DialogResultEventArgs(true));
-        Close();
-    }
 
 
-    private void TextBox_LostFocus(object sender, RoutedEventArgs e)
-    {
-        TextBox textBox = (TextBox)sender;
-        BindingExpression bindingExpr = textBox.GetBindingExpression(TextBox.TextProperty);
-
-        // Manually trigger the validation
-        bindingExpr.UpdateSource();
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
     }
 }
+
